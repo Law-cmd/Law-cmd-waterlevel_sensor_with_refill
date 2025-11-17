bbe4314228f3cd9cfa267b029b1710714d4e7684
Ahmad_ALI <kojoa5666@gmail.com>
2025-11-17 16:14:33 +0100

    setup v0

--- waterlevel_sensor_with_refill.git/src/main.cpp
+++ waterlevel_sensor_with_refill.git/src/main.cpp
@@ -1,13 +1,18 @@
 #include <Arduino.h>
 #include <Servo.h>
 #include <LiquidCrystal_I2C.h>
+   
+Servo servo;
+int angle = 10;
 
 void setup() {
-  
+    Serial.begin(115200);
+    servo.attach(8);
+    servo.write(angle);
 }
 
 void loop() {
-  
+   static int water_level;
 }
 
 

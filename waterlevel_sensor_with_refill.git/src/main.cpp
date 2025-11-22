4de444db5b042de09534f313fca1f64513349943
Ahmad Ali <kojoa5666@gmail.com>
2025-11-22 18:51:37 +0100

    V1

--- waterlevel_sensor_with_refill.git/src/main.cpp
+++ waterlevel_sensor_with_refill.git/src/main.cpp
@@ -1,18 +1,115 @@
 #include <Arduino.h>
 #include <Servo.h>
 #include <LiquidCrystal_I2C.h>
+
+
+byte waterdrop[8] =
+{
+0b00000,
+0b00100,
+0b01110,
+0b11111,
+0b11111,
+0b11111,
+0b01110,
+0b00000
+};
+byte smile[8] =
+{
+0b00000,
+0b00000,
+0b01010,
+0b00000,
+0b10001,
+0b01110,
+0b00000,
+0b00000
+};
+
+
+
+#define sensorPower 10  //watersensor
+Servo servo; int angle;//servo
+/*funktionen*/
+int readSensor();
+void setColor(int, int, int) ;
+/*funktionen*/
+
+int redPin= 7;
+int greenPin = 6;
+int  bluePin = 5;
+
+LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x3F for a 16 chars and 2 line display
+
+
+void setup() 
+{
+  lcd.createChar(0, waterdrop);
+  lcd.createChar(1, smile);
+  lcd.init();
+  lcd.clear();         
+  lcd.backlight();   
+
+  pinMode(A1, OUTPUT);//sensor
+
+  pinMode(11, OUTPUT);//piezo
+
+  servo.attach(8);
+  servo.write(angle);
+
+  digitalWrite(sensorPower, LOW);
+  pinMode(redPin, OUTPUT); pinMode(greenPin, OUTPUT); pinMode(bluePin, OUTPUT);
+}
+
+void loop() 
+{   
+
+    int waterlevel = readSensor();
+     
    
-Servo servo;
-int angle = 10;
-
-void setup() {
-    Serial.begin(115200);
-    servo.attach(8);
+    if(waterlevel<=100)
+    {  
+     lcd.setCursor(2,0); 
+     lcd.print("waterlevel low starting refill");
+     lcd.scrollDisplayLeft();   // scroll everything to the left by one position
+     lcd.setCursor(0, 1);
+     lcd.write(0);
+     tone(11, 500);delay(1);noTone(11);
+     setColor(255,0 ,0 );
+     delay(100);
+     angle=90 ;
+      
+    }
+    else
+    {  
+     setColor(0,255,0);  
+     lcd.clear();
+     lcd.setCursor(2,0); 
+     lcd.print("waterlevel: ");lcd.print(waterlevel);
+     lcd.scrollDisplayLeft();   // scroll everything to the left by one position
+     lcd.write(1);
+     delay(100); 
+     angle=0;
+    
+    }
+    
     servo.write(angle);
+     
+}   
+
+
+
+void setColor(int redValue, int greenValue,  int blueValue) 
+{
+ analogWrite(redPin, redValue);
+ analogWrite(greenPin,  greenValue);
+ analogWrite(bluePin, blueValue);
 }
-
-void loop() {
-   static int water_level;
-}
-
-
+ int readSensor() 
+ {
+  digitalWrite(sensorPower, HIGH);  // Turn the sensor ON
+  delay(10);                        // wait 10 milliseconds
+  int val = analogRead(A1);      // Read the analog value form sensor
+  digitalWrite(sensorPower, LOW);   // Turn the sensor OFF
+  return val; 
+ }
